4c95e401417087282f995c24021e3872f5388e2a
R-L-Rayford <rebecca.rayford@yahoo.com>
2019-12-10 11:43:05 -0500

    added folders for file organization

--- submit-to-email.php
+++ submit-to-email.php
@@ -4,4 +4,15 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $phone = $_POST['number'];
 $message = $_POST['message'];
+
+$to = "rrayford@emich.edu";
+$subject = "New Web Inquiry!";
+$header = "From: $email";
+$body = "You have received a new inquiry from :\n\nNAME:\t\t ".$name."\nEMAIL: ".$email."\n\nMESSAGE:\t\t ".$message;
+
+//make changes to script once I have a 'thank you' page. Will also need to add 'error' page to else block
+
+if(mail($to, $subject, $body, $header)){
+	echo "<script language='javascript'>alert('E-mail sent!'');window.location='http://breezeflooring.com/index.html';</script>";
+}
 ?>
